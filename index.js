76e0465ee3c523e7c8e0e2316bb487d414dd2d76
Kalsie Holt <kalsie@student.dev-island.org>
2023-09-04 20:26:18 -1000

    Edit html and css, add javascript functions

---
+++ index.js
@@ -0,0 +1,33 @@
+const passwordInput = document.querySelector("#input_password");
+const confirmPassword = document.querySelector("#confirm_password");
+const inputs = document.querySelectorAll("input");
+const submitBtn = document.querySelector(".submit");
+
+const checkPassword = () => {
+  const matchError = document.querySelector(".match_error");
+
+  if (passwordInput.value === "" || confirmPassword.value === "") {
+    passwordInput.classList.add("error");
+    confirmPassword.classList.add("error");
+  } else if (passwordInput.value !== confirmPassword.value) {
+    confirmPassword.classList.add("error");
+    matchError.style.display = "block";
+  } else {
+    passwordInput.classList.remove("error");
+    confirmPassword.classList.remove("error");
+  }
+};
+
+const validate = () => {
+  inputs.forEach((input) => {
+    if (input.value === "") {
+      input.classList.add("error");
+    } else {
+      input.classList.remove("error");
+    }
+  });
+};
+
+submit.addEventListener("click", validate);
+passwordInput.addEventListener("input", checkPassword);
+confirmPassword.addEventListener("input", checkPassword);
